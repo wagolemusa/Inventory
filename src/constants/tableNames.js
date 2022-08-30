5163727e6340828d151c23f88573a78b7b91b8c3
wagolemusa <homiemusa@gmail.com>
2022-08-30 22:57:08 +0300

    Create new tables

--- src/constants/tableNames.js
+++ src/constants/tableNames.js
@@ -1,10 +1,12 @@
 module.exports = {
-    user: 'user',
-    item_type: 'item_type',
-    state: 'state',
-    country: 'country',
-    shape: 'shape',
-    location: 'location',
+    employee: 'employee',
+    role: 'role',
+    items: 'items',
+    shop: 'shop',
+    order: 'order',
     address: 'address',
-    manufacturer: 'manufacturer',
+    customer: 'customer',
+    points: 'points',
+    payment: 'payment',
+    tracker: 'Tracker'
 };
